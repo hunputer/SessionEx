5530f1dd7bb1474412724cb2abc517f46d1692d8
hunputer <wlgns3855@gmail.com>
2023-08-24 22:12:53 +0900

    sessionEx2

--- src/main/java/com/example/sessionEx/controller/MainController.java
+++ src/main/java/com/example/sessionEx/controller/MainController.java
@@ -4,9 +4,12 @@ import com.example.sessionEx.service.MainService;
 import com.example.sessionEx.util.SessionUtil;
 import com.example.sessionEx.vo.UserVO;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.websocket.Session;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
@@ -16,33 +19,35 @@ import java.util.List;
 public class MainController {
 
     private final MainService mainService;
-    private final SessionUtil sessionUtil;
 
     @GetMapping("/")
-    public String main(){
+    public String main(HttpServletRequest request){
 
-        List<UserVO> userList = mainService.getUser();
+        if(!SessionUtil.isLogin(request)){
+            return "redirect:./loginReg";
+        }
 
-        System.out.println("");
+        UserVO userVO = SessionUtil.getUser(request);
+        request.setAttribute("user", userVO);
 
         return "mainForm";
     }
 
-    @GetMapping("/login")
-    public void login(HttpServletRequest httpServletRequest, @RequestBody UserVO userVO){
+    @GetMapping("/loginReg")
+    public String loginRegForm(HttpServletRequest request){
+        return "loginForm";
+    }
+
+    @PostMapping("/login")
+    public void login(HttpServletRequest request, @RequestBody UserVO userVO){
         List<UserVO> userList = mainService.getUser();
         UserVO user = userList.get(0);
 
-        SessionUtil.login(httpServletRequest, user);
+        SessionUtil.login(request, user);
     }
 
-    @GetMapping("/logout")
-    public void logout(HttpServletRequest httpServletRequest){
-        SessionUtil.logout(httpServletRequest);
-    }
-
-    @GetMapping("/getUser")
-    public void getUser(HttpServletRequest httpServletRequest){
-        SessionUtil.logout(httpServletRequest);
+    @PostMapping("/logout")
+    public void logout(HttpServletRequest request){
+        SessionUtil.logout(request);
     }
 }
