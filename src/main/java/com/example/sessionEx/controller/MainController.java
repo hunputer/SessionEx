fd5e5f9228c911730cee0edf71732acaf00fc4da
hunputer <wlgns3855@gmail.com>
2023-08-24 22:00:41 +0900

    sessionEx2

--- src/main/java/com/example/sessionEx/controller/MainController.java
+++ src/main/java/com/example/sessionEx/controller/MainController.java
@@ -1,10 +1,13 @@
 package com.example.sessionEx.controller;
 
 import com.example.sessionEx.service.MainService;
+import com.example.sessionEx.util.SessionUtil;
 import com.example.sessionEx.vo.UserVO;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -13,6 +16,7 @@ import java.util.List;
 public class MainController {
 
     private final MainService mainService;
+    private final SessionUtil sessionUtil;
 
     @GetMapping("/")
     public String main(){
@@ -24,4 +28,21 @@ public class MainController {
         return "mainForm";
     }
 
+    @GetMapping("/login")
+    public void login(HttpServletRequest httpServletRequest, @RequestBody UserVO userVO){
+        List<UserVO> userList = mainService.getUser();
+        UserVO user = userList.get(0);
+
+        SessionUtil.login(httpServletRequest, user);
+    }
+
+    @GetMapping("/logout")
+    public void logout(HttpServletRequest httpServletRequest){
+        SessionUtil.logout(httpServletRequest);
+    }
+
+    @GetMapping("/getUser")
+    public void getUser(HttpServletRequest httpServletRequest){
+        SessionUtil.logout(httpServletRequest);
+    }
 }
--- src/main/java/com/example/sessionEx/util/SessionUtil.java
+++ src/main/java/com/example/sessionEx/util/SessionUtil.java
@@ -1,4 +1,30 @@
 package com.example.sessionEx.util;
 
+import com.example.sessionEx.vo.UserVO;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
 public class SessionUtil {
+
+    public static void login(HttpServletRequest v1, UserVO userVo){
+        HttpSession session = v1.getSession(true);
+        synchronized (session){
+            session.setAttribute("user", userVo);
+        }
+    }
+
+    public static boolean isLogin(HttpServletRequest v1){
+        HttpSession session = v1.getSession(true);
+        return session.getAttribute("user") != null ? true : false;
+    }
+
+    public static UserVO getUser(HttpServletRequest v1){
+        HttpSession session = v1.getSession(true);
+        return (UserVO)session.getAttribute("user");
+    }
+
+    public static void logout(HttpServletRequest v1){
+        HttpSession session = v1.getSession(true);
+        session.invalidate();
+    }
 }
